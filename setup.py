5016c84c38cd5a56d47c919535c72633576b0ac3
Thon Sopheap <t.sopheap@dgc.gov.kh>
2026-03-17 16:17:49 +0700

    bump version 3.22.3-rc

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if os.geteuid() == 0:
         os.makedirs('/var/lib/wo/tmp/')
 
 setup(name='wordops',
-      version='3.22.2-rc',
+      version='3.22.3-rc',
       description='An essential toolset that eases server administration',
       long_description=LONG,
       long_description_content_type='text/markdown',
